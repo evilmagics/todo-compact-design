9b1ed182c9070d26eca5942b6ae4050dac4830af
EvilMagics <richy.rotuahta15@gmail.com>
2024-09-17 17:23:11 +0700

    Update: Add example code

---
+++ main.go
@@ -0,0 +1,11 @@
+// [ ] Uncompleted but not mandatory or not important
+// [#] Uncompleted or want to do and must to do
+// [X] Error or don't do it
+// [!] Bug in the code and need fixed
+// [*] Bug in the code and must fixed first, this is mandatory error or bug
+// [?] Unknown function or feature or not implemented yet
+// [T] Completed and prepare to testing
+// [~] Completed and need documentation
+// [+] Completed and ready to use
+
+package main
